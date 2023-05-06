7feb99bd28e6d5fbff661caab2679bb5c7c36083
DenysMkl <den.mikliaiev@gmail.com>
2023-05-06 16:45:45 +0300

    first commit

---
+++ jsCode/script.js
@@ -0,0 +1,40 @@
+times = Array.from(document.querySelectorAll('.board > *'))
+
+let mass = {
+
+}
+
+console.log(times)
+
+times.forEach(element => {
+    list = Array.from(element.querySelectorAll('.number')).map(val=>val.textContent)
+    mass[element.classList.value] = list.join('')
+});
+
+function TransferToSeconds(h, m, s) {
+    transSeconds = Number(h) * 3600 + Number(m)* 60 + Number(s)
+    
+    return transSeconds
+}
+
+totalTime = TransferToSeconds(mass['hours'], mass['minutes'], mass['seconds'])
+console.log(totalTime)
+timer = setInterval(()=>{
+    totalTime-=1
+    newHours = String(Math.floor(totalTime / 3600))
+    newMinutes = String(Math.floor((totalTime % 3600) / 60)) 
+    newSeconds = String(Math.floor(totalTime % 60))
+    resH = '0'.repeat(mass['hours'].length - newHours.length) + newHours
+    resM = '0'.repeat(mass['minutes'].length - newMinutes.length) + newMinutes
+    resS = '0'.repeat(mass['seconds'].length - newSeconds.length) + newSeconds
+    newTime = [resH, resM, resS]
+    
+    times.forEach(elem=>{
+        index = times.indexOf(elem)
+        list = Array.from(elem.querySelectorAll('.number'))
+        list.forEach(value=>{
+            value.textContent = newTime[index][list.indexOf(value)]
+        })
+    })
+
+}, 1000)
